4b5a81a52ccfffb12a698f4a6c13304a2a938e4d
k4s4s <k4s4s.heavener@gmail.com>
2013-02-04 13:55:49 +0100

    changed apply function, in order to compile with older g++ versions

--- futures/apply_function.hpp
+++ futures/apply_function.hpp
@@ -12,13 +12,10 @@ namespace functor_utils {
 
 template<size_t N>
 struct Apply {
-    template<typename F, typename T, typename... A>
-    static inline auto apply(F&& f, T && t, A &&... a)
-        -> decltype(Apply<N-1>::apply(::std::forward<F>(f), ::std::forward<T>(t),
-            ::std::get<N-1>(::std::forward<T>(t)), ::std::forward<A>(a)...
-        ))
+    template<typename F, typename RT, typename T, typename... A>
+    static inline void apply(F&& f, RT *retVal, T && t, A &&... a)
     {
-        return Apply<N-1>::apply(::std::forward<F>(f), ::std::forward<T>(t),
+        Apply<N-1>::apply(::std::forward<F>(f), retVal, ::std::forward<T>(t),
             ::std::get<N-1>(::std::forward<T>(t)), ::std::forward<A>(a)...
         );
     }
@@ -26,23 +23,19 @@ struct Apply {
 
 template<>
 struct Apply<0> {
-    template<typename F, typename T, typename... A>
-    static inline auto apply(F && f, T &&, A &&... a)
-        -> decltype(f(::std::forward<A>(a)...))
+    template<typename F, typename RT, typename T, typename... A>
+    static inline void apply(F && f, RT* retVal, T &&, A &&... a)
     {
-        return f(::std::forward<A>(a)...);
+        *retVal = f(::std::forward<A>(a)...);
     }
 };
 
-template<typename F, typename T>
-inline auto apply(F && f, T && t)
-     -> decltype(Apply< ::std::tuple_size<
-         typename ::std::decay<T>::type
-     >::value>::apply(::std::forward<F>(f), ::std::forward<T>(t)))
+template<typename F, typename RT, typename T>
+inline void apply(F && f,  RT *retVal, T && t)
 {
-    return Apply< ::std::tuple_size<
+    Apply< ::std::tuple_size<
         typename ::std::decay<T>::type
-    >::value>::apply(f, ::std::forward<T>(t));
+    >::value>::apply(f, retVal, ::std::forward<T>(t));
 };
 
 }
